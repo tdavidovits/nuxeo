493ef8daa349d4824ef726fe18fa7b2d39f765fa
Vladimir Pasquier <vpasquier@nuxeo.com>
2015-02-09 20:43:35 +0100

    NXP-16453: fix leak in automation trace cache (store only one trace per chain/operation)

--- nuxeo-features/nuxeo-automation/nuxeo-automation-core/src/main/java/org/nuxeo/ecm/automation/core/trace/TracerFactory.java
+++ nuxeo-features/nuxeo-automation/nuxeo-automation-core/src/main/java/org/nuxeo/ecm/automation/core/trace/TracerFactory.java
@@ -95,6 +95,10 @@ public class TracerFactory implements TracerFactoryMBean {
             traces.clear();
         }
 
+        protected int size() {
+            return traces.size();
+        }
+
     }
 
     /**
@@ -135,6 +139,10 @@ public class TracerFactory implements TracerFactoryMBean {
         }
         if (trace.getError() != null) {
             lastError = trace;
+        }
+        chainTraces = tracesCache.getIfPresent(chainId);
+        if (chainTraces.size() != 0) {
+            chainTraces.removeTrace(1);
         }
         return tracesCache.getIfPresent(chainId).add(trace);
     }
